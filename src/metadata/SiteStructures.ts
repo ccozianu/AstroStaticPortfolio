acd26640800a4cb3de42dac11fceca95cee9cbcf
Costin Cozianu <ccozianu@gmail.com>
2026-03-18 05:24:08 +0200

    20260318 WIP

---
+++ src/metadata/SiteStructures.ts
@@ -0,0 +1,18 @@
+export const mainSections = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "Projects",
+        href: "/projects",
+    },
+    {
+        name: "Blog",
+        href: "/blog",
+    },
+    {
+        name: "About",
+        href: "#contact",
+    },
+]
